d6182abefb1838f4a5c7d32600e6cfe0f8be8b31
Phinn Sriployrung <psrip001@gmail.com>
2022-03-21 19:28:11 -0700

    added Users component and reorganized component tree

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import React from 'react';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 
 class App extends React.Component {
   render() {
     return (
-      <nav className='navbar bg-primary'>
-        <Navbar title='Github Finder' icon='fab fa-github' />
-      </nav>
+      <div>
+        <nav className='navbar bg-primary'>
+          <Navbar title='Github Finder' icon='fab fa-github' />
+        </nav>
+        <div className='container'>
+          <Users />
+        </div>
+      </div>
     );
   }
-
-}
+};
 
 export default App;
