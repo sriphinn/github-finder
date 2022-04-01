fc2a0c14d12e8bee9d254b514f3cce3437128b43
Phinn Sriployrung <psrip001@gmail.com>
2022-03-31 18:41:33 -0700

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 import axios from 'axios';
-
+import './App.css';
 
 class App extends React.Component {
   state = {
@@ -66,21 +67,30 @@ class App extends React.Component {
   render() {
     const { users, loading, alert } = this.state
     return (
+      <Router>
       <div>
-        <nav className='navbar bg-primary'>
-          <Navbar title='Github Finder' icon='fab fa-github' />
-        </nav>
+        <Navbar title='Github Finder' icon='fab fa-github' />
         <div className='container'>
           <Alert alert={alert} />
-          <Search 
-            searchUsers={this.searchUsers} 
-            clearUsers={this.clearUsers} 
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+          <Routes>
+              <Route  
+                path='/' 
+                element={
+                <Fragment>
+                  <Search 
+                    searchUsers={this.searchUsers} 
+                    clearUsers={this.clearUsers} 
+                    showClear={users.length > 0 ? true : false}
+                    setAlert={this.setAlert}
+                  />
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              } />
+              <Route path='/about' element={<About />} />
+          </Routes>
         </div>
       </div>
+      </Router>
     );
   }
 };
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ icon, title }) => {
   
   //destructing the props from function means we don't have to 'props.' when calling the props
   
   return (
-      <div>
+      <nav className='navbar bg-primary'>
         <h1>
           <i className={icon} />
            {title}
         </h1>
-      </div>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+        </ul>
+      </nav>
     )
-}
+};
 
 Navbar.defaultProps = {
   title: 'Github Finder',
